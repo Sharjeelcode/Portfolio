7e4c34459fad566a5f3982f1fd12f64d559e3bc7
Sharjeel Ahmed <107884305+Sharjeelcode@users.noreply.github.com>
2024-05-19 00:03:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -227,7 +227,7 @@ function App() {
                   </a>
                   <a
                     target="blank"
-                    href="https://profolio-2-airbnbclone.netlify.app/"
+                    href="https://projectairbnbclone.netlify.app/"
                   >
                     <button class="group flex justify-center items-center gap-2 group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-neutral-900 duration-500 hover:duration-500 underline underline-offset-2 hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-neutral-300 relative bg-neutral-900 px-4 py-4 border text-left p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden after:absolute after:z-10 after:w-12 after:h-12 after:content[''] after:bg-sky-900 after:-left-8 after:top-8 after:rounded-full after:blur-lg hover:after:animate-pulse">
                       preview
