a63b17a83c67ccdfc4fadf9ae74af7538dd1850c
unknown <sharjeelahmed3808@gmail.com>
2024-05-22 16:19:42 +0500

    one  more project added and projects yui updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import github from "../src/assets/github.png";
 import sass from "../src/assets/sass.png";
 import Project1 from "./assets/project1.png";
 import Project2 from "./assets/project2.png";
+import Project3 from "./assets/project3.png";
 import LinkedIn from "./assets/linkedin.svg";
 import Instagram from "./assets/instagram.svg";
 import ArrowDown from "./assets/arrow-down.svg";
@@ -143,8 +144,8 @@ function App() {
         {/* Projects section */}
         <section id="projects">
           <div className="container m-auto px-4 sm:py-12">
-            <h2 className="text-2xl font-semibold ">Projects</h2>
-            <div className="flex flex-col sm:flex-row gap-10 mt-11">
+            <h2 className="text-2xl font-semibold mt-4 lg:mt-0">Projects</h2>
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 md:gap-3  lg:gap-3 mt-11">
               <div
                 data-aos="flip-left"
                 class="md:m-12 bg-[#171F38]  shadow-[#209CE4] shadow-xl border-2  border-[#209CE4] rounded-xl hover:scale-110 duration-700 p-5"
@@ -197,9 +198,9 @@ function App() {
                 <img src={Project2} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">Airbnb Clone</h3>
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive Developed Airbnb Clone application using Tailwind
-                  CSS, React.js and Firebase. Implemented user authentication,
-                  property listings, and real-time updates
+                  Developed a responsive Airbnb Clone application using Tailwind
+                  CSS, React.js, and Firebase. Implemented user authentication,
+                  property listings, and real-time updates.
                 </p>
                 <div class="pt-5 pb-2 flex gap-2 justify-center">
                   <a
@@ -228,6 +229,51 @@ function App() {
                   <a
                     target="blank"
                     href="https://profolio-2-airbnbclone.netlify.app/"
+                  >
+                    <button class="group flex justify-center items-center gap-2 group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-neutral-900 duration-500 hover:duration-500 underline underline-offset-2 hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-neutral-300 relative bg-neutral-900 px-4 py-4 border text-left p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden after:absolute after:z-10 after:w-12 after:h-12 after:content[''] after:bg-sky-900 after:-left-8 after:top-8 after:rounded-full after:blur-lg hover:after:animate-pulse">
+                      preview
+                    </button>
+                  </a>
+                </div>
+              </div>
+              <div
+                data-aos="flip-right"
+                class="md:m-12 bg-[#171F38] shadow-[#209CE4] shadow-xl border-[#209CE4]  border-2  rounded-xl hover:scale-110 duration-700 p-5"
+              >
+                <img src={Project3} className="w-full h-auto" />
+                <h3 className="text-2xl font-semibold mt-8">FinTrackr</h3>
+                <p className="text-gray-400 text-sm mt-2">
+                  Developed FinTrackr Expense Management system using HTML, CSS,
+                  Tailwind, and JavaScript. Designed to help users efficiently
+                  track their income and expenses.
+                </p>
+                <div class="pt-5 pb-2 flex gap-2 justify-center">
+                  <a
+                    target="blank"
+                    href="https://github.com/Sharjeelcode/IEC_Profolio_1"
+                  >
+                    <button class="group flex justify-center items-center gap-2 group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-neutral-900 duration-500 hover:duration-500 underline underline-offset-2 hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-neutral-300 relative bg-neutral-900 px-4 py-4 border text-left p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden after:absolute after:z-10 after:w-12 after:h-12 after:content[''] after:bg-sky-900 after:-left-8 after:top-8 after:rounded-full after:blur-lg hover:after:animate-pulse">
+                      <svg
+                        class="w-6 h-6 fill-neutral-50"
+                        height="100"
+                        preserveAspectRatio="xMidYMid meet"
+                        viewBox="0 0 100 100"
+                        width="100"
+                        x="0"
+                        xmlns="http://www.w3.org/2000/svg"
+                        y="0"
+                      >
+                        <path
+                          class="svg-fill-primary"
+                          d="M50,1.23A50,50,0,0,0,34.2,98.68c2.5.46,3.41-1.09,3.41-2.41s0-4.33-.07-8.5c-13.91,3-16.84-6.71-16.84-6.71-2.28-5.77-5.55-7.31-5.55-7.31-4.54-3.1.34-3,.34-3,5,.35,7.66,5.15,7.66,5.15C27.61,83.5,34.85,81.3,37.7,80a10.72,10.72,0,0,1,3.17-6.69C29.77,72.07,18.1,67.78,18.1,48.62A19.34,19.34,0,0,1,23.25,35.2c-.52-1.26-2.23-6.34.49-13.23,0,0,4.19-1.34,13.75,5.13a47.18,47.18,0,0,1,25,0C72.07,20.63,76.26,22,76.26,22c2.72,6.89,1,12,.49,13.23a19.28,19.28,0,0,1,5.14,13.42c0,19.21-11.69,23.44-22.83,24.67,1.8,1.55,3.4,4.6,3.4,9.26,0,6.69-.06,12.08-.06,13.72,0,1.34.9,2.89,3.44,2.4A50,50,0,0,0,50,1.23Z"
+                        ></path>
+                      </svg>
+                      Github
+                    </button>
+                  </a>
+                  <a
+                    target="blank"
+                    href="hhttps://main--tiny-cheesecake-69f047.netlify.app/"
                   >
                     <button class="group flex justify-center items-center gap-2 group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-neutral-900 duration-500 hover:duration-500 underline underline-offset-2 hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-neutral-300 relative bg-neutral-900 px-4 py-4 border text-left p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden after:absolute after:z-10 after:w-12 after:h-12 after:content[''] after:bg-sky-900 after:-left-8 after:top-8 after:rounded-full after:blur-lg hover:after:animate-pulse">
                       preview
